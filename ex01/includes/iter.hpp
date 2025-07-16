a4fbc8da70e810a5eb7535d7bb06bc34cbbc44f8
tsurma <tsurma@student.42berlin.de>
2025-07-16 14:00:34 +0200

    added double typenaming for const support

--- ex01/includes/iter.hpp
+++ ex01/includes/iter.hpp
@@ -1,17 +1,19 @@
 #ifndef ITER_HPP_
 #define ITER_HPP_
 
-template<typename T>
-void iter(T* adress, int len, void (*ptr)(T*)) {
-	for (int i = 0; i < len; ++i) {
-		ptr(&(adress[i]));
+#include <iostream>
+#include <stdio.h>
+
+template<typename T, typename Func>
+void iter(T* adress, size_t len, Func func_ptr) {
+	for (size_t i = 0; i < len; ++i) {
+		func_ptr(adress[i]);
 	}
 }
 
-template<typename T>
-void increment(T* position) {
-	(*position)++;
+template <typename T>
+void printData(const T& data) {
+	putchar(data);
 }
-
 
 #endif
